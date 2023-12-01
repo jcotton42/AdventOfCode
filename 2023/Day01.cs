88f2f0e7b55bff2c6c3f0bbce996463197e35fe4
Joshua Cotton <jcotton42@outlook.com>
2023-12-01 11:10:44 -0800

    Start.

---
+++ 2023/Day01.cs
@@ -0,0 +1,16 @@
+using AdventOfCodeSupport;
+
+namespace AdventOfCode._2023;
+
+public sealed class Day01 : AdventBase
+{
+    protected override object InternalPart1()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override object InternalPart2()
+    {
+        throw new NotImplementedException();
+    }
+}
